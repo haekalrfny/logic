f3c465e70776b6f5d36261b1c2c6f940fa6a4a2a
haekalrfny <haekalrfny@gmail.com>
2023-02-07 09:17:42 +0700

    logika

---
+++ latihan-5.js
@@ -0,0 +1,12 @@
+let n = 9
+
+for (let x = 1; x <= n; x++){
+        for (let y = 1; y <= n; y++){
+            if (y % 2 == 0) {
+                document.write('-', ' ')
+            } else {
+                document.write('+ &nbsp' )
+            }
+        }
+        document.write('<br>')
+    }
